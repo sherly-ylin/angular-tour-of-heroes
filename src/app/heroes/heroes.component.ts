aa82ff1600c7cb5a140436c59bf586f8dad1c07c
Yixuan Sherly Lin <linyixuansherly@gmail.com>
2024-01-22 23:11:33 -0500

    select-list

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,14 +1,25 @@
 import { Component } from '@angular/core';
 
 import { Hero } from '../hero'
+import { HEROES } from '../mock-heroes';
+
+import{
+  NgFor
+} from '@angular/common'
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrl: './heroes.component.css'
 })
 export class HeroesComponent {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+  // hero: Hero = {
+  //   id: 1,
+  //   name: 'Windstorm'
+  // };
+  heroes = HEROES;
+  // ? - the member is optional
+  selectedHero?: Hero;
+  onSelect(hero: Hero): void{
+    this.selectedHero = hero;
+  }
 }
